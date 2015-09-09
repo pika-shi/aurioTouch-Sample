0c3e953c6e86dae83578e119e77a4b51d2af0c39
Takemura Hikaru <takemura@dl.kuis.kyoto-u.ac.jp>
2015-09-10 08:54:53 +0900

    initial commit

---
+++ aurioTouch-Sample/aurioTouch-Sample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  aurioTouch-Sample-Bridging-Header.h
+//  aurioTouch-Sample
+//
+//  Created by Hikaru Takemura on 2015/09/10.
+//  Copyright (c) 2015年 Hz. All rights reserved.
+//
+
+#ifndef aurioTouch_Sample_aurioTouch_Sample_Bridging_Header_h
+#define aurioTouch_Sample_aurioTouch_Sample_Bridging_Header_h
+
+# import "AudioController.h"
+
+#endif
---
+++ aurioTouch-SampleTests/aurioTouch_SampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  aurioTouch_SampleTests.swift
+//  aurioTouch-SampleTests
+//
+//  Created by Hikaru Takemura on 2015/09/10.
+//  Copyright (c) 2015年 Hz. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class aurioTouch_SampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
